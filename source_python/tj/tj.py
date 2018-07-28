983f2b09d4db8f8c026143fa02c068bc979e978b
lokki7 <olyanifig@ya.ru>
2018-07-28 21:45:37 +0300

    first commit

--- source_python/tj/tj.py
+++ source_python/tj/tj.py
@@ -20,34 +20,9 @@ def unload():
     SayText2('Plugin has been unloaded successfully!').send()
 
 
-@Event('player_spawn')
-def on_player_spawn(event):
-    SayText2('player_spawn').send()
-
-
-@Event('start_halftime')
-def start_halftime(event):
-    SayText2('start_halftime').send()
-
-
-@Event('cs_intermission')
-def cs_intermission(event):
-    SayText2('cs_intermission').send()
-
-
-@Event('map_transition')
-def map_transition(event):
-    SayText2('map_transition').send()
-
-
-@Event('player_score')
-def player_score(event):
-    SayText2('player_score').send()
-
-
-@Event('team_score')
-def player_score(event):
-    SayText2('team_score').send()
+@Event('round_end')
+def round_end(event):
+    SayText2('round_end').send()
 
 
 @Event('cs_win_panel_match')
