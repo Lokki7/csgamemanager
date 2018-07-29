aac6dd0d041c5f050e6a65da37121c744000eae1
lokki7 <olyanifig@ya.ru>
2018-07-29 15:58:57 +0300

    first commit

--- index.mjs
+++ index.mjs
@@ -1,6 +1,6 @@
 import express from 'express';
 import CsServer from './cs-server';
-import fetch from 'node-fetch';
+import request from 'request';
 
 const app = express();
 app.use(express.json());
@@ -35,13 +35,21 @@ app.post('/cs/ended', async (req, res) => {
     score: score
   };
 
-  fetch('http://esport-tj.imt.zone/api/matches/duels/result', {
-    method: 'POST',
-    body:    JSON.stringify(body),
-    headers: { 'Content-Type': 'application/json' },
-  })
-    .then(res => res.json())
-    .then(json => console.log(json));
+  request.post({
+    url: 'http://esport-tj.imt.zone/api/matches/duels/result',
+    json: body
+  }, function (error, response, body) {
+    console.log(response, body)
+  });
+
+
+  // fetch(, {
+  //   method: 'POST',
+  //   body:    JSON.stringify(),
+  //   headers: { 'Content-Type': 'application/json' },
+  // })
+  //   .then(res => res.json())
+  //   .then(json => console.log(json));
 
 
   // send score to web
